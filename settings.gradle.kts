49393f22c7f80fc823aeb424755cc84a2be1e0b9
GitHub Action <action@github.com>
2022-08-30 22:06:40 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("pastelmusic")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
